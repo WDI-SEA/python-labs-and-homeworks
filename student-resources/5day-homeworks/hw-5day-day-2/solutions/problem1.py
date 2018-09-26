0182f5d1f80b4c101f4018e1b083a4ef6f5ddaee
DeusMalsith <justinrashallphotography@gmail.com>
2018-09-26 09:05:32 -0700

    Homework

--- student-resources/5day-homeworks/hw-5day-day-2/solutions/problem1.py
+++ student-resources/5day-homeworks/hw-5day-day-2/solutions/problem1.py
@@ -1,8 +1,19 @@
 def get_contact(contacts, name):
-    if name in contacts:
-        return contacts[name]
-    else:
-        return "You don't have that person's number!"
+	if name in contacts:
+		print(f'The phone number of {name} is {contacts[name]}')
+	else:
+		print(f'{name} is not in the contacts')
+
+contacts = {
+  "Carly": "333-3333",
+  "Blondie": "444-4444",
+  "Jenny": "867-5309"
+}
+
+phone_number = get_contact(contacts, 'Jenny')
+phone_number = get_contact(contacts, 'Carly')
+phone_number = get_contact(contacts, 'Blondie')
+phone_number = get_contact(contacts, 'Justin')
 
 
 # If you would like to test the example code, uncomment the code below
--- student-resources/5day-homeworks/hw-5day-day-2/solutions/problem4.py
+++ student-resources/5day-homeworks/hw-5day-day-2/solutions/problem4.py
@@ -1,84 +1,79 @@
 class Animal:
   def __init__(self):
-    print("I'm coming to life!")
+    print('I am coming to life!')
     self.energy = 50
-
-  def eat(self, amount):
+  
+  def eat(self,amount):
     self.energy += amount
 
   def move(self):
     self.energy -= 10
-    print("I am running!")
-
-  def say_hi(self):
-    print("Meep!")
+    print('I am running!')
 
   def get_status(self):
-    print("My energy level is", self.energy)
-
-    if self.energy < 0:
-      print("I'm starving!")
-    elif self.energy < 50:
-      print("I'm getting hungry!")
-    elif self.energy <= 100:
-      print("I'm happily full.")
+    message = ''
+    if (self.energy < 0):
+      message = "I'm starving!"
+    elif (self.energy in range (0,50)):
+      message = "I'm getting hungry!"
+    elif (self.energy in range (50, 101)):
+      message = "I'm happily full."
     else:
-      print("I'm feeling stuffed!")
+      message = "I'm feeling stuffed!"
+    print(f'Current energy is {self.energy}.')
+    print(message)
+
+  def say_hi(self):
+    print('Meep!')
 
 class Penguin(Animal):
   def __init__(self):
     super().__init__()
-    print("I am a penguin!")
     self.energy = 100
+    print("I am a penguin!")
 
   def move(self):
-    self.energy -= 5
+    self.energy -= 20
     print("I am sliding!")
 
 class Eagle(Animal):
   def __init__(self):
     super().__init__()
-    print("I am an eagle!")
     self.energy = 20
 
+  def say_hi(self):
+    print("shrieeeek!")
+
   def move(self):
-    if self.energy < 0:
+    self.energy -= 20
+    if (self.energy < 0):
       print("I'm too tired to fly...")
     else:
       print("I am flying to the sea!")
 
-    self.energy -= 20
+animal = Animal()
+animal.get_status()
+animal.eat(60)
+animal.get_status()
+animal.move()
+animal.get_status()
+animal.say_hi()
+print()
 
-  def say_hi(self):
-    print("Shrieeeeek!")
+penguin = Penguin()
+penguin.eat(5)
+penguin.get_status()
+penguin.move()
+penguin.get_status()
+penguin.say_hi()
+print()
 
-
-
-# If you want to test the example, uncomment the code below
-
-# animal = Animal()
-# animal.get_status()
-# animal.eat(60)
-# animal.get_status()
-# animal.move()
-# animal.get_status()
-# animal.say_hi()
-# print()
-
-# penguin = Penguin()
-# penguin.eat(5)
-# penguin.get_status()
-# penguin.move()
-# penguin.get_status()
-# penguin.say_hi()
-# print()
-
-# eagle = Eagle()
-# eagle.say_hi()
-# eagle.get_status()
-# eagle.move()
-# eagle.get_status()
-# eagle.move()
-# eagle.get_status()
-# eagle.move()
-# print()
+eagle = Eagle()
+eagle.say_hi()
+eagle.get_status()
+eagle.move()
+eagle.get_status()
+eagle.move()
+eagle.get_status()
+eagle.move()
+print()
